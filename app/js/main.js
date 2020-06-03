10fb323cd4b08f94d864ae8c8a679eb5154fbb52
MikhailZheltovskikh <misha14051994@mail.ru>
2020-06-04 01:07:59 +0500

    added upload.html

--- app/js/main.js
+++ app/js/main.js
@@ -53,7 +53,9 @@ $ (function(){
             $(this).addClass('active');
             $('#'+id).addClass('active-tab').fadeIn();
             return false;
-        });
+    });
+
+    $('input, select').styler();
     
 
     var mixer = mixitup('.products__inner-box');
